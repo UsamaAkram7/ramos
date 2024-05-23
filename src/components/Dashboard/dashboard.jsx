cc96c4d90ce0c658a879c4921fd2f9ef6518a471
osamaakram <usamaakram85@gmail.com>
2024-05-24 02:45:51 +0500

    sjhsjs

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -4,21 +4,24 @@ import "./dashboard.scss";
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
-      <div className="main-text">
-          <div class="image-container">
-            <img class="image1" src="/thunder.png" />
-            <img class="image2" src="/line-chart.png" />
+        <div className="main-container">
+          <div className="container1">
+            <div class="image-container1">
+              <img class="image1" src="/thunder.png" />
+              <img class="image2" src="/line-chart.png" />
+            </div>
+            <div className="text1">Analytics that helps you</div>
           </div>
-          <div className="text1"> Analytics</div>
-          <div class="image-container">
+          <div class="image-container1">
             <img className="image3" src="play.png" />
             <img class="image4" src="/laptop.PNG" />
           </div>
-         <div>that helps you</div>
-          <div className="text3">shape </div>
+        </div>
+        <div className="text2">
+          shape
           <img class="image5" src="/bar-chart.png" />
-          <div className="text4">the future</div>
-      </div>
+          your future
+        </div>
     </div>
   );
 };
